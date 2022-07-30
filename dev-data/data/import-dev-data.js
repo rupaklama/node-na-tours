494acc8a5feb74380db6611dea92cd3fe190e1ce
pak <rupaklamadev@gmail.com>
2022-07-29 21:33:07 -0400

    created custom scripts to create/delete data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,78 @@
+/* NOTE - this will run completely independent from Express app & 
+  only going to run once in the beginning to populate data in our data base
+*/
+
+// to read our local file
+const fs = require('fs');
+
+// tour model to populate with data
+
+/* Server related */
+const mongoose = require('mongoose');
+
+// to access Env variables
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+// object to specify the path to our config file
+// this will set env variables in Node process module
+dotenv.config({ path: './config.env' });
+
+// connect to database
+const db = process.env.DATABASE;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // connection object
+    // console.log(con.connections);
+    console.log('DB connection successful!');
+  });
+
+// read json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// import data into db
+const importData = async () => {
+  try {
+    // create method always accepts array of objects
+    await Tour.create(tours);
+    console.log('Data successfully created!');
+  } catch (err) {
+    console.error(err);
+  }
+
+  // to terminate the process synchronously with an exit status of code
+  process.exit();
+};
+
+// delete all data from collection
+const deleteData = async () => {
+  try {
+    // to delete all of the documents in the collection
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.error(err);
+  }
+
+  process.exit();
+};
+
+// process.argv property returns an array containing the command-line
+// arguments passed when the Node.js process was launched
+// console.log(process.argv);  default scripts
+
+// note - creating our custom Scripts
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
--- models/tourModel.js
+++ models/tourModel.js
@@ -9,16 +9,78 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     // to be unique
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    // enum: {
+    //   values: ['easy', 'medium', 'difficult'],
+    //   message: 'Difficulty is either: easy, medium, difficult',
+    // },
+  },
+  ratingsAverage: {
     type: Number,
     // to set default value
     default: 4.5,
+    // min: [1, 'Rating must be above 1.0'],
+    // max: [5, 'Rating must be below 5.0'],
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: {
+    type: Number,
+    // validate: {
+    //   validator: function (val) {
+    //     // this only points to current doc on NEW document creation
+    //     return val < this.price;
+    //   },
+    //   message: 'Discount price ({VALUE}) should be below regular price',
+    // },
+  },
+  summary: {
+    type: String,
+    // to remove white spaces in start and end
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  // array of strings
+  images: [String],
+  createdAt: {
+    // javascript built in data type - Date object
+    type: Date,
+    // To get a current Timestamp of right now
+    // In mongoose, this will be immidiately converted to current date
+    default: Date.now(),
+    // select: false,
+  },
+  startDates: [Date],
+  // secretTour: {
+  //   type: Boolean,
+  //   default: false,
+  // },
 });
 
 // convention to always use uppercase for Modal Names & related variables
